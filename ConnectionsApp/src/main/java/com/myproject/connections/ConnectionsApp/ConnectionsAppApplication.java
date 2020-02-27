a6a9b5a20b6c93c2b8802a9509b317621ca85c7d
Shridha <shridha.jalihal@gmail.com>
2020-02-27 21:37:56 +0100

    testing

---
+++ ConnectionsApp/src/main/java/com/myproject/connections/ConnectionsApp/ConnectionsAppApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.connections.ConnectionsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectionsAppApplication.class, args);
+	}
+
+}
---
+++ ConnectionsApp/src/test/java/com/myproject/connections/ConnectionsApp/ConnectionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.connections.ConnectionsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
