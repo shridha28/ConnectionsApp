aad373235e858e146aae9b65f2bca4545ccfe878
Shridha <shridha.jalihal@gmail.com>
2020-02-28 15:20:44 +0100

    created components and view pages

--- Connections-ng-app/src/app/app.module.ts
+++ Connections-ng-app/src/app/app.module.ts
@@ -2,13 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { LoginsignupComponent } from './loginsignup/loginsignup.component';
+import {Router,Routes, RouterModule} from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+import {FormsModule} from '@angular/forms';
+import { config } from 'rxjs';
 
+const appRoutes:Routes=[
+  {
+    path:'loginsignup',
+    component:LoginsignupComponent
+  },
+  {
+    path:'feedback',
+    component:FeedbackComponent
+  },
+  {
+    path:'',
+    component:LoginsignupComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    component:NotfoundComponent
+  },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FeedbackComponent,
+    LoginsignupComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes,{enableTracing:true}),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Connections-ng-app/src/app/loginsignup/loginsignup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginsignup',
+  templateUrl: './loginsignup.component.html',
+  styleUrls: ['./loginsignup.component.css']
+})
+export class LoginsignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
