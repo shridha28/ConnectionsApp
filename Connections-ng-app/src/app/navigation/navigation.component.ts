baf1609cc15f2966b1d71b5c5f7370ece52b4136
Shridha <shridha.jalihal@gmail.com>
2020-03-28 20:29:37 +0100

    Fixed navigation to activities tab and made routing a module

--- Connections-ng-app/src/app/navigation/navigation.component.ts
+++ Connections-ng-app/src/app/navigation/navigation.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ChangeDetectorRef } from '@angular/core';
+import {DataServiceService} from '../services-shared/data-service.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnInit{
 
-  constructor() { }
+  isUserLoggedIn: boolean;
+  constructor(private dataSharingService: DataServiceService,
+    private changeDetectorRef: ChangeDetectorRef) {
+    
+   }
 
   ngOnInit(): void {
+    this.dataSharingService.isUserLoggedIn.subscribe( value => {
+      this.isUserLoggedIn = value;
+      this.changeDetectorRef.detectChanges();
+    }); 
   }
 
 }
